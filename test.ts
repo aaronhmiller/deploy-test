fe704808f701c3641df9071eb2520b8c151f99e6
Aaron Miller <aaronmiller@gmail.com>
2024-09-13 19:53:00 -0700

    more isolation

--- test.ts
+++ test.ts
@@ -1,3 +1,8 @@
+import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+
+const app = new Application();
+const router = new Router();
+
 console.log("Server running...");
 console.log("This is very very unique to see if ISOLATE is it...");
 
